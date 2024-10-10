97c59ac6ff6e6713c668bca4aa4960ac47eef6ab
kubashh <146523311+kubashh@users.noreply.github.com>
2024-10-10 18:43:38 +0000

    0.2.7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { BrowserRouter } from "react-router-dom"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { App } from "./App"
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter>
     <App />
